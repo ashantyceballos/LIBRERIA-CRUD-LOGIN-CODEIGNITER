cf79753f83f690d886e8dd6a42a4baa097f78e1c
ashantyceballos <80500302+ashantyceballos@users.noreply.github.com>
2021-03-24 23:28:02 -0500

    Initial commit

---
+++ app/Views/template/pie.php
@@ -0,0 +1,14 @@
+        </div>
+    </body>
+    <footer class="my-5 pt-5 text-muted text-center text-small" >
+        <p class="mb-1">&copy; 2021 Biblioteca Ashanty Lizeth Ceballos Pech</p>
+        <ul class="list-inline">
+            <li class="list-inline-item"><a href="#">Privacidad</a></li>
+            <li class="list-inline-item"><a href="#">Términos</a></li>
+            <li class="list-inline-item"><a href="#">Soporte</a></li>
+        </ul>
+    </footer>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</html>
